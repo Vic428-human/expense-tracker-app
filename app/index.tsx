1640d19f706806ef609e63258e66eb32d9f9e8bf
Vic428-human <z0983195379a@gmail.com>
2025-04-17 19:40:59 +0800

    1.定義根路由 2.Header Title 隱藏 3.styling container for index

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,22 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { colors } from '@/constants/theme'
 
 const index = () => {
   return (
-    <View>
-      <Text>index</Text>
+    <View style={styles.container}>
+      <Text>index!!</Text>
     </View>
   )
 }
 
 export default index
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        justifyContent:'center',
+        alignItems:'center',
+        backgroundColor:colors.neutral900
+    }
+})
