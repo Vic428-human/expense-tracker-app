b970791f265a3bae29ca333020a2ca3dbfee5e7b
Vic428-human <z0983195379a@gmail.com>
2025-04-17 20:41:24 +0800

    製作一開始的轉場畫面，兩秒後進入Welcome頁面

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,19 @@
 import { StyleSheet, Text, View, Image } from 'react-native'
-import React from 'react'
+import { useRouter } from 'expo-router';
+import React, { useEffect } from 'react'
 import { colors } from '@/constants/theme'
 
+
 const index = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      router.push("/(auth)/welcome");
+    }, 2000); // 2秒後跳轉
+
+    return () => clearTimeout(timer);
+  }, []);
   return (
     <View style={styles.container}>
       <Image
@@ -17,14 +28,14 @@ const index = () => {
 export default index
 
 const styles = StyleSheet.create({
-    container:{
-        flex:1,
-        justifyContent:'center',
-        alignItems:'center',
-        backgroundColor:colors.neutral900
-    },
-    logo:{
-        height: '20%',
-        aspectRatio: 1, // 確保圖片寬高都是1:1,不會因為設備不同而變形
-    }
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: colors.neutral900
+  },
+  logo: {
+    height: '20%',
+    aspectRatio: 1, // 確保圖片寬高都是1:1,不會因為設備不同而變形
+  }
 })
