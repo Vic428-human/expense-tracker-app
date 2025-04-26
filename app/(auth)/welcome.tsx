a26bd4079383ccbdd0b7b3256964ed73b915d800
Vic428-human <z0983195379a@gmail.com>
2025-04-26 14:03:22 +0800

    切版：1. 登入位置 2.圖片位置

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -1,19 +1,49 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import React from "react";
 import ScreenWrapper from "@/components/ScreenWrapper";
 import Typo from "@/components/Typo";
-import { colors } from "@/constants/theme";
+import { colors, spacingX, spacingY } from "@/constants/theme";
+import { verticalScale } from "@/utils/styling";
 
 const welcome = () => {
   return (
     <ScreenWrapper>
-      <Typo size={30} fontWeight={"700"} color={colors.rose}>
-        welcome
-      </Typo>
+      <View style={styles.container}>
+        <View>
+          <TouchableOpacity style={styles.logginBUtton}>
+            <Typo fontWeight={"500"}>Sign In</Typo>
+          </TouchableOpacity>
+          <Image
+            source={require("../../assets/images/welcome.png")}
+            resizeMode="contain"
+            style={styles.welcomeImage}
+          />
+        </View>
+
+        <Typo size={30} fontWeight={"700"} color={colors.rose}>
+          welcome!
+        </Typo>
+      </View>
     </ScreenWrapper>
   );
 };
 
 export default welcome;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "space-between", // 預設是上下對齊
+    padding: spacingY._7,
+  },
+  logginBUtton: {
+    alignSelf: "flex-end",
+    marginRight: spacingX._20,
+  },
+  welcomeImage: {
+    width: "100%",
+    height: verticalScale(300),
+    alignSelf: "center",
+    marginTop: verticalScale(100),
+  },
+});
