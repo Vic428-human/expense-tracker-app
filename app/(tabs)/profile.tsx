06b16d1b24bd6146c4b920f49be5b4dbc83c8925
Vic428-human <z0983195379a@gmail.com>
2025-04-30 07:11:52 +0800

    profile lists 切版

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,16 +1,44 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import React from "react";
 import ScreenWrapper from "@/components/ScreenWrapper";
 import Typo from "@/components/Typo";
-import { colors, spacingX, spacingY } from "@/constants/theme";
+import { colors, radius, spacingX, spacingY } from "@/constants/theme";
 import Header from "@/components/Header";
 import BackButton from "@/components/BackButton";
 import { verticalScale } from "@/utils/styling";
 import { useAuth } from "@/context/authContext";
 import { Image } from "expo-image";
+import { getProfileImage } from "@/services/imageService";
+import { accountOptionProps } from "@/types";
+import * as Icon from "phosphor-react-native";
 
 const Profile = () => {
   const { user } = useAuth();
+
+  // 創造一個物件陣列，物件有好幾個，每個物件都有 accountOptionProps 裡面具備的屬性
+  const accountOptions: accountOptionProps[] = [
+    {
+      title: "Edit Profile",
+      icon: <Icon.User />,
+      bgColor: "#6366f1",
+      routeName: "/(modals)/profileModal",
+    },
+    {
+      title: "Setting",
+      icon: <Icon.GearSix />,
+      bgColor: "#059669",
+    },
+    {
+      title: "Privacy Policy",
+      icon: <Icon.Lock />,
+      bgColor: colors.neutral600,
+    },
+    {
+      title: "Logout",
+      icon: <Icon.Power />,
+      bgColor: "#e11d48",
+    },
+  ];
   return (
     <ScreenWrapper>
       <View style={styles.container}>
@@ -28,7 +56,7 @@ const Profile = () => {
             {/* https://docs.expo.dev/versions/latest/sdk/image/ */}
             <Image
               style={styles.avatar}
-              source={user?.image}
+              source={getProfileImage(user?.image)}
               // placeholder={{ blurhash }}
               contentFit="cover"
               transition={1000}
@@ -43,6 +71,33 @@ const Profile = () => {
               {user?.email}
             </Typo>
           </View>
+        </View>
+        {/* account option */}
+        <View style={styles.accountOption}>
+          {accountOptions.map((option, index) => (
+            <View style={styles.listItem} key={index}>
+              <TouchableOpacity style={styles.flexRow}>
+                <View
+                  style={[
+                    styles.listIcon,
+                    { backgroundColor: option?.bgColor },
+                  ]}
+                >
+                  {option.icon && option.icon}
+                </View>
+                {/*這邊加了flex 1 不曉得為什麼沒有對齊 */}
+                <Typo size={16} fontWeight={"500"}>
+                  {option?.title}
+                </Typo>
+                <Icon.CaretRight
+                  size={verticalScale(20)}
+                  weight="bold"
+                  color={colors.white}
+                  style={{ marginLeft: "auto" }} // 這一行讓箭頭靠右
+                />
+              </TouchableOpacity>
+            </View>
+          ))}
         </View>
       </View>
     </ScreenWrapper>
@@ -67,5 +122,26 @@ const styles = StyleSheet.create({
     borderRadius: 200,
     backgroundColor: colors.neutral300,
     alignSelf: "center",
+  },
+  accountOption: {
+    marginTop: spacingY._35,
+  },
+  listItem: {
+    marginBottom: verticalScale(17),
+    justifyContent: "center",
+  },
+  flexRow: {
+    flexDirection: "row",
+    gap: spacingX._10,
+    alignItems: "center",
+  },
+  listIcon: {
+    width: verticalScale(44),
+    height: verticalScale(44),
+    backgroundColor: colors.neutral500,
+    borderRadius: radius._15,
+    borderCurve: "continuous",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
