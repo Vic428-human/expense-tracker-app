c3670516b31cc94ab6ae73ac50b9667dfe166ecf
Vic428-human <z0983195379a@gmail.com>
2025-04-29 20:08:26 +0800

    添加tabs的路由 2.對profile點選時隱藏 3.引用客製化組件tabs，做法是根據官方做法

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,11 +1,34 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
-import { Tabs } from "expo-router";
+import { Tabs, useSegments } from "expo-router";
+import CustomTabs from "@/components/CustomTabs";
 
 const _layout = () => {
+  const segment = useSegments();
+
+  // 假設進入到 profile 則把 tabs 隱藏 https://github.com/expo/router/discussions/313
+  const hideProfileTabBar = [...segment].includes("profile");
+
   return (
-    <Tabs>
-      <Text>_layout</Text>
+    // https://docs.expo.dev/router/advanced/tabs/
+    <Tabs
+      screenOptions={{ tabBarActiveTintColor: "blue", headerShown: false }} // 這裡得設定會被 CustomTabs裡的覆蓋
+      tabBar={hideProfileTabBar ? () => null : CustomTabs}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "主頁",
+        }}
+      />
+      <Tabs.Screen name="wallet" options={{ title: "錢包" }} />
+      <Tabs.Screen name="profile" options={{ title: "個人" }} />
+      <Tabs.Screen
+        name="statistic"
+        options={{
+          title: "統計",
+        }}
+      />
     </Tabs>
   );
 };
