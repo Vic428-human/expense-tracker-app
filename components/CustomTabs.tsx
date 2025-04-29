9e13e3859ae513bcb0fa5074848b68e6a5d4de49
Vic428-human <z0983195379a@gmail.com>
2025-04-29 20:09:58 +0800

    基於官方文檔做法製作客製化的Bottomtabs切版設計

---
+++ components/CustomTabs.tsx
@@ -0,0 +1,130 @@
+import { View, StyleSheet, TouchableOpacity, Platform } from "react-native";
+import { useLinkBuilder, useTheme } from "@react-navigation/native";
+import { Text, PlatformPressable } from "@react-navigation/elements";
+import {
+  BottomTabBarProps,
+  createBottomTabNavigator,
+} from "@react-navigation/bottom-tabs";
+import { colors, spacingY } from "@/constants/theme";
+import { verticalScale } from "@/utils/styling";
+import * as Icons from "phosphor-react-native";
+
+// 這裡的範本引用的是官方文檔
+// https://reactnavigation.org/docs/bottom-tab-navigator/
+export default function CustomTabs({
+  state,
+  descriptors,
+  navigation,
+}: BottomTabBarProps) {
+  // 下面這兩段不需要，顏色用自己客制化的即可
+  //   const { colors } = useTheme();
+  //   const { buildHref } = useLinkBuilder();
+
+  // create tabbar for multiple icons
+  const tabBarIcons: any = {
+    // 參考
+    //  ({ color }) => <FontAwesome size={28} name="home" color={color} />,
+    // 以上面這個為基礎，要多個的 每個 key 對應一筆 ({ color }) => <FontAwesome size={28} name="home" color={color} />
+    index: (isFocused: boolean) => (
+      <Icons.House
+        size={verticalScale(30)}
+        weight={isFocused ? "fill" : "regular"}
+        color={isFocused ? colors.primary : colors.text}
+      />
+    ),
+    wallet: (isFocused: boolean) => (
+      <Icons.Wallet
+        size={verticalScale(30)}
+        weight={isFocused ? "fill" : "regular"}
+        color={isFocused ? colors.primary : colors.text}
+      />
+    ),
+    profile: (isFocused: boolean) => (
+      <Icons.User
+        size={verticalScale(30)}
+        weight={isFocused ? "fill" : "regular"}
+        color={isFocused ? colors.primary : colors.text}
+      />
+    ),
+    statistic: (isFocused: boolean) => (
+      <Icons.ChartBar
+        size={verticalScale(30)}
+        weight={isFocused ? "fill" : "regular"}
+        color={isFocused ? colors.primary : colors.text}
+      />
+    ),
+  };
+
+  return (
+    // 補上寬高
+    <View style={styles.tabbar}>
+      {state.routes.map((route, index) => {
+        const { options } = descriptors[route.key];
+        const label: any =
+          options.tabBarLabel !== undefined
+            ? options.tabBarLabel
+            : options.title !== undefined
+            ? options.title
+            : route.name;
+
+        const isFocused = state.index === index;
+
+        const onPress = () => {
+          const event = navigation.emit({
+            type: "tabPress",
+            target: route.key,
+            canPreventDefault: true,
+          });
+
+          if (!isFocused && !event.defaultPrevented) {
+            navigation.navigate(route.name, route.params);
+          }
+        };
+
+        const onLongPress = () => {
+          navigation.emit({
+            type: "tabLongPress",
+            target: route.key,
+          });
+        };
+
+        return (
+          <TouchableOpacity
+            key={route.key}
+            accessibilityState={isFocused ? { selected: true } : {}}
+            accessibilityLabel={options.tabBarAccessibilityLabel}
+            testID={options.tabBarButtonTestID}
+            onPress={onPress}
+            onLongPress={onLongPress}
+            style={styles.tabBarItem}
+          >
+            <Text
+              style={{ color: isFocused ? colors.primary : colors.neutral800 }}
+            >
+              {/* route.name 對應的是 Tabs.Screen name= */}
+              {tabBarIcons[route.name] && tabBarIcons[route.name](isFocused)}
+            </Text>
+          </TouchableOpacity>
+        );
+      })}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  tabbar: {
+    flexDirection: "row",
+    width: "100%",
+    height: Platform.OS === "ios" ? verticalScale(73) : verticalScale(55),
+    backgroundColor: colors.neutral800,
+    justifyContent: "space-around",
+    alignItems: "center",
+    borderTopColor: colors.rose,
+    borderTopWidth: 1,
+  },
+  tabBarItem: {
+    marginBottom: Platform.OS === "ios" ? spacingY._10 : spacingY._5,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
