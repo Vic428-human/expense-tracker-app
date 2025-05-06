00b1d648992d9d539927a3ab730f41d2092cc948
Vic428-human <z0983195379a@gmail.com>
2025-05-06 20:31:48 +0800

    用onSnapshot的方式把firebase上的wallets collection 快取下來

--- hooks/useFetchData.ts
+++ hooks/useFetchData.ts
@@ -1,6 +1,6 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React, { useEffect, useState } from 'react'
-import { collection, query, where, getDocs, QueryConstraint } from "firebase/firestore";
+import { collection, query, onSnapshot, getDocs, QueryConstraint } from "firebase/firestore";
 import { db } from '@/config/firebase';
 
 // T 代表一个“类型参数”，就像函数参数一样，但它用于类型。这样写的目的是让你的函数、接口或组件在定义时不预先指定具体的数据类型，而是在使用时再指定类型，从而增强代码的灵活性和复用性
@@ -9,14 +9,32 @@ import { db } from '@/config/firebase';
 const useFetchData = <T>(collectionName:string, contraints: QueryConstraint[]) => {
     const [data, setData] = useState<T[]>([]);
     const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(null);
+    const [error, setError] = useState<string|null>(null);
 
     useEffect(()=>{
         if(!collectionName) return;
         const collectionRef = collection(db, collectionName); // 會指向資料庫中的某個位置。無論資料是否存在，您都可以建立參照
-        // 可以使用 where() 查詢符合特定條件的所有文件 where 其實就是 QueryConstraint 的其中一種 https://firebase.google.com/docs/firestore/query-data/get-data?hl=zh-tw
+        // 可以使用 where() 查詢符合特定條件的所有文件 where 其實就是 QueryConstraint 的其中一種。 https://firebase.google.com/docs/firestore/query-data/get-data?hl=zh-tw
         // ex: const q = query(collectionRef, where("capital", "==", true));
         const q = query(collectionRef, ...contraints);
+
+        // 標題： 處理監聽錯誤 => https://firebase.google.com/docs/firestore/query-data/listen?hl=zh-tw
+        const unsubscribe = onSnapshot(q, (snapshot) => {
+            // Respond to data
+            const fetchData = snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id })) as T[];
+            console.log("Current data: ", );
+            setData(fetchData);
+            setLoading(false);
+        },(error) => {
+            console.log('Error getting documents: ', error);
+            setError(error.message)
+            setLoading(false);
+        });
+
+        // Later ...
+
+        // Stop listening to changes => https://firebase.google.com/docs/firestore/query-data/listen?hl=zh-tw
+        return () => unsubscribe();
     })
 
     return (
